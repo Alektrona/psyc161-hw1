f55c68c79a233dfee73822efd6dbd706495451d5
Katherine Alfred <katherinealfred.dartmouth@gmail.com>
2015-04-04 18:23:06 -0400

    Wrote factorial command and tests, fixed bugs, checked style

--- factorial.py
+++ factorial.py
@@ -1,24 +1,28 @@
-"""Module for estimation of factorial (Homework #1)
-
-Note:  this is just a skeleton for you to work with.  But it already
-       has some "bugs" you need to catch and fix.
-"""
+# Katherine Alfred Homework 1, 4/4/2015
 
 from nose.tools import assert_equal
 
+
+# Counts activations of factorial_recursive in stack until it hits the
+# base case, where it'll return up the stack, multiplying n as it goes
 def factorial_recursive(n):
-    # TODO Define your logic for factorial here
-    pass # does nothing
+    if (n == 0):
+        return 1
+    else:
+        return n * factorial_recursive(n-1)
+
 
 def test_factorial():
     assert_equal(factorial_recursive(1), 1)
-    # TODO: add more
+    assert_equal(factorial_recursive(3), 6)
+    assert_equal(factorial_recursive(6), 720)
+
 
 if __name__ == '__main__':
     # This is a way to determine either file was "executed", so if it was
     # imported (by e.g. nose) as a library, we should not run code
     # below
 
-    nconditions = raw_input("Please enter number of conditions: ")
+    nconditions = int(raw_input("Please enter number of conditions: "))
     norders = factorial_recursive(nconditions)
-    print("Number of possible trial orders: " + str(norders)
+    print("Number of possible trial orders: " + str(norders))
